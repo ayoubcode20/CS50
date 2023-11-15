31ac45be3e545d8b0615328486225425746dfe0b
Ayoub <aoumraniayoub658@gmail.com>
2023-11-15 09:03:05 -0800

    bulbs.c 100%

---
+++ bulbs.c
@@ -0,0 +1,44 @@
+#include <cs50.h>
+#include <stdio.h>
+#include <string.h>
+
+const int BITS_IN_BYTE = 8;
+
+void print_bulb(int bit);
+
+int main(void)
+{
+    // TODO
+    int i, j, ch, k;
+    int arr[BITS_IN_BYTE];
+
+    string str = get_string("Message: ");
+
+    for (i = 0; str[i]; i++)
+    {
+        ch = str[i];
+        for (j = 0; j < BITS_IN_BYTE; j++)
+        {
+            arr[j] = ch % 2;
+            ch /= 2;
+        }
+        for (k = BITS_IN_BYTE - 1; k >= 0; k--)
+            print_bulb(arr[k]);
+        printf("\n");
+    }
+
+}
+
+void print_bulb(int bit)
+{
+    if (bit == 0)
+    {
+        // Dark emoji
+        printf("\U000026AB");
+    }
+    else if (bit == 1)
+    {
+        // Light emoji
+        printf("\U0001F7E1");
+    }
+}
